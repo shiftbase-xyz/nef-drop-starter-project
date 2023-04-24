74807bca46b90e9a2739029d8028f0a012073632
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-04-24 23:37:28 +0900

    chore: delete comment out

--- @types/window.d.ts
+++ @types/window.d.ts
@@ -1,13 +1,3 @@
-// // import { MetaMaskInpageProvider } from '@metamask/providers';
-// import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-
-// declare global {
-//   interface Window {
-//     // ethereum: MetaMaskInpageProvider;
-//     solana: PhantomWalletAdapter;
-//   }
-// }
-
 interface Window {
   solana: any;
 }
